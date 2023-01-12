3e995845147b97c612b1543735290f6b067af740
amarogu <gumarinho090306@gmail.com>
2023-01-12 15:42:09 -0300

    active nav completed

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,18 @@
+import "animate.css";
+
 const span = document.getElementById("nav-icon3");
 const header = document.getElementById("header");
+const items = document.querySelectorAll(".item");
+const qr = document.querySelector(".qr");
 span.addEventListener("click", () => {
   span.classList.toggle("open");
   header.classList.toggle("max-h-[70]");
   header.classList.toggle("max-h-screen");
+  items.forEach((item) => {
+    item.classList.toggle("animate__animated");
+    item.classList.toggle("animate__flipInX");
+    qr.classList.toggle("animate__animated");
+    qr.classList.toggle("animate__fadeInDown");
+  });
   // header.classList.toggle("items-center");
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,22 @@ module.exports = {
       colors: {
         body: "#202020",
         lightgray: "#bcbcbc",
+        extralightgray: "#202020bf",
+      },
+      fontFamily: {
+        body: ["Inter", "ui-sans-serif", "system-ui"],
+      },
+      fontSize: {
+        sm: "12px",
+        base: "14px",
+        lg: "16px",
+        "2lg": "18px",
+        "3lg": "20px",
+        "4lg": "25px",
+        xl: "30px",
+        "2xl": "35px",
+        "3xl": "40px",
+        "4xl": "45px",
       },
     },
   },
